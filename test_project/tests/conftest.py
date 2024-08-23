5d4e43dd15606b909d9bec28e0b6a3f79549bc67
MarieAmbrazhei <marykapuza@gmail.com>
2024-08-23 19:18:16 +0300

    add disable-gpu

--- test_project/tests/conftest.py
+++ test_project/tests/conftest.py
@@ -13,6 +13,7 @@ def browsers_chrome(request):
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--disable-gpu')
 
     drivers = []
 
